4fc9496576103e36d9f73127518cf011468ca95b
yangddoddi <pud1251@gmail.com>
2022-01-23 19:37:02 +0900

    JS_ButtonScroll

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 "strict";
 
-// navbar
+// When scrolling down the page, switch the navigation bar from transparent to smoke white.
 
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-console.log(navbarHeight);
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(navbarHeight);
   if (window.scrollY >= navbarHeight) {
     navbar.classList.add("navbar__grey");
   } else {
     navbar.classList.remove("navbar__grey");
   }
+});
+
+// Pressing the menu button scrolls to the corresponding page.
+
+const navbarMenu = document.querySelector(".navbar__menu");
+console.log(navbarMenu);
+navbarMenu.addEventListener("click", (event) => {
+  const Target = event.target;
+  const Position = Target.dataset.link;
+  if (Position == null) {
+    return;
+  }
+  const Link = document.querySelector(Position);
+  Link.scrollIntoView({ behavior: "smooth" });
+});
+
+// Pressing the Contact Me button moves to the contact item.
+
+const Home = document.querySelector("#home");
+Home.addEventListener("click", (event) => {
+  const ContactMe = document.querySelector("#contact");
+  ContactMe.scrollIntoView({ behavior: "smooth" });
 });
