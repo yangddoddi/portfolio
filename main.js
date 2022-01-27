a7522b7b875e323c4abeadd72253d17ef48e2c4e
yangddoddi <pud1251@gmail.com>
2022-01-28 00:57:42 +0900

    JS_NavbarActive

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ navbarMenu.addEventListener("click", (event) => {
   navbarMenu.classList.remove("open");
   const Link = document.querySelector(Position);
   Link.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(Target);
 });
 
 // Navbar toggle button
@@ -35,12 +36,70 @@ hamburger.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
+// 1. 모든 섹션 요소들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonial",
+  "#contact",
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
+const ObserverOption = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, ObserverOption);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
 // Pressing the Contact Me button moves to the contact item.
 
 const Contacbtn = document.querySelector(".home__contact");
 Contacbtn.addEventListener("click", (event) => {
-  const ContactMe = document.querySelector("#contact");
-  ContactMe.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView("#contact");
 });
 
 // Home screen fade out.
@@ -68,6 +127,7 @@ document.addEventListener("scroll", () => {
 function scrollIntoView(selector) {
   const Scrollto = document.querySelector(selector);
   Scrollto.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 ArrowButton.addEventListener("click", () => {
